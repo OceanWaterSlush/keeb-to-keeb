a0777ba998b92f1e79be4e17ae1eac2b0f5d10d8
TommyMynnSon <tommy.m.son@gmail.com>
2023-10-01 17:34:48 -0700

    Add type detection for environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,13 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+interface SupabaseEnv {
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
+}
+
+interface ImportMetaEnv extends SupabaseEnv {}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
